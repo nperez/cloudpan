a16860cf3b17decf5be9411dffc8036295c5ba68
nperez <nperez@cpan.org>
2012-06-21 14:21:33 +0200

    Add docs

--- lib/CloudPAN.pm
+++ lib/CloudPAN.pm
@@ -1,4 +1,7 @@
 package CloudPAN;
+
+#ABSTRACT: Never install pure Perl modules again
+
 use warnings;
 use strict;
 
@@ -37,3 +40,26 @@ BEGIN {
 }
 
 1;
+__END__
+=head1 SYNOPSIS
+
+    use CloudPAN;
+    
+    {
+        package Foo;
+        use Moo;
+
+        has bar => (is => 'rw');
+        
+        sub baz { $_[0]->bar }
+    }
+    print Foo->new(bar => 3)->baz . "\n";
+    
+    # 3
+
+=head1 DESCRIPTION
+Ever wanted to load modules from the "cloud"? Love the concept of MetaCPAN and
+want to exercise it? Then this module is for you. Simply use this module before
+using any other module that doesn't require compilation and you're set. Note
+that this doesn't work on all modules (especially ones that mess around with
+@INC too). 
